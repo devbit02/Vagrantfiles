73b3411db885bfd76f248858b153ec16406aa039
Roger Brown <rbrown@Gotransverses-MacBook-Pro-2.local>
2018-12-31 12:47:49 -0600

    Adding awscli-vagrant development box

---
+++ awscli-vagrant/README.md
@@ -0,0 +1,10 @@
+# Vagrant core setup
+
+This development enviornment has the following:
+* Ubuntu
+* Python
+* Docker
+* git
+* make
+* curl
+* vim
---
+++ awscli-vagrant/scripts/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo apt-get update --fix-missing
+sudo apt-get install -y python-pip
+sudo apt-get install -q -y make git curl vim
+sudo apt-get 
+pip install awscli
